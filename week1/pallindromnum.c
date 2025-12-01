654994171bd3fbed0c0b0ef702b60c88527e9e1d
ben <bmwangi676@gmail.com>
2025-12-01 00:23:48 -0500

    minimal edits

--- week1/pallindromnum.c
+++ week1/pallindromnum.c
@@ -17,7 +17,7 @@ int main(void){
 bool is_pallindrome(int x){
 
     if (x < 0){
-        
+
         return false;
     }
     int sum = 0;
@@ -26,7 +26,6 @@ bool is_pallindrome(int x){
         int rem = x % 10;
         sum = sum * 10 + rem;
         x /= 10;
-        printf("sum: %d rem: %d x: %d\n", sum, rem, x);
     }
     return sum == original;
 }
