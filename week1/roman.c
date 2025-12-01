35ba9279699bf6e7d0c31730bd23e4dd0be354e5
ben <bmwangi676@gmail.com>
2025-12-01 00:25:10 -0500

    faster solution to roman converter

--- week1/roman.c
+++ week1/roman.c
@@ -3,6 +3,8 @@
 #include <stdlib.h>
 #include <ctype.h>
 
+/*
+slow solution  
 int romanToInt(char* s){
    int res = 0;
    int pre = 0;
@@ -24,6 +26,37 @@ int romanToInt(char* s){
    }
    return res;
 }
+*/
+
+//fast solution
+int value(char c){
+    switch(c){
+        case 'I': return 1;
+        case 'V': return 5;
+        case 'X': return 10;
+        case 'L': return 50;
+        case 'C': return 100;
+        case 'D': return 500;
+        case 'M': return 1000;
+        default: return 0;
+    }
+}
+
+int romanToInt(char* s){
+    int sum = 0;
+
+    for(int i = 0; s[i] != '\0'; i++){
+        int curr = value(s[i]);
+        int next = value(s[i+1]);
+
+        if(curr < next)
+            sum -= curr;
+        else
+            sum += curr;
+    }
+    return sum;
+}
+
 
 char *get_string(char *prompt) {
     char temp[100];
